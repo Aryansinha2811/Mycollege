89f49d2039825ff1a0e233d387ffd67b7ae0d32b
Aryansinha2811 <sinhaaryan622@gmail.com>
2023-07-01 11:38:31 +0530

    Adding 4 more files of C practicals into My College Repo

---
+++ star_pyramid.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() 
+{
+    // Declare variables i, j, n, and k
+    int i, j, n = 4, k;
+    
+    // Outer loop for the rows
+    // Starts from n and goes down to 1
+    for (i = n; i >= 1; i--) 
+    {
+        
+        // Inner loop to print spaces
+        // Starts from i and goes down to 2
+        for (j = i; j >= 2; j--) {
+            printf(" "); // Print a space
+        }
+        
+        // Inner loop to print asterisks
+        // Number of asterisks depends on the row number
+        // Starts from 1 and goes up to 2 * (n - i + 1) - 1
+        for (k = 1; k <= 2 * (n - i + 1) - 1; k++) {
+            printf("*"); // Print an asterisk
+        }
+        
+        printf("\n"); // Print a new line after each row
+    }
+    
+    return 0;
+}
